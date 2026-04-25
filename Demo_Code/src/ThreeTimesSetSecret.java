d355fc9fbe6c55ef10c46b3e119ac1e11a927190
Adams Conan Doyle <3662308525@qq.com>
2026-04-25 11:05:29 +0800

    重构三层加密，实现long类型加密

--- Demo_Code/src/ThreeTimesSetSecret.java
+++ Demo_Code/src/ThreeTimesSetSecret.java
@@ -6,15 +6,15 @@ public class ThreeTimesSetSecret {
     //自然底数
     public final double e=Math.E;
     //实现数组反转
-    public int[] reverse(int[] nums){
-        int[]store=new int[nums.length];
+    public long[] reverse(long[] nums){
+        long[]store=new long[nums.length];
         for(int i=nums.length-1;i>=0;i--) store[nums.length-1-i]=nums[i];
         return store;
     }
 
     //求解数字长度
     public int length(int n){
-        if(n<0) return -1;
+        if(n<0) return length(-n);
         if(n<10) return 1;
         int count=0;
         do {
@@ -33,19 +33,19 @@ public class ThreeTimesSetSecret {
     }
 
     //int类型转换为数组
-    public int[]parse(int n){
-        int[]arr=new int[length(n)];
+    public long[]parse(long n){
+        long[]arr=new long[length((int) n)];
         for(int i=0;i<arr.length;i++){
-            arr[i]=n/pow(10,length(n)-1);
-            n=n-pow(10,length(n)-1)*arr[i];
+            arr[i]=n/pow(10,length((int) n)-1);
+            n=n-pow(10,length((int) n)-1)*arr[i];
         }
         return arr;
     }
 
     //概率
-    public int[] result1(int s,int[]originalSecret){
+    public long[] result1(long s,long[]originalSecret){
 
-        int[] secret=parse(s);//最终待反转的密码，s参数是密码的int类型
+        long[] secret=parse(s);//最终待反转的密码，s参数是密码的int类型
         int[]secretKey=new int[secret.length];//密钥
         Random r=new Random();
         if(originalSecret.length==secret.length) { //设计密钥
@@ -55,7 +55,7 @@ public class ThreeTimesSetSecret {
                 secret[i] = originalSecret[i] - secretKey[i];
             }
             //反转后的密码数组与密钥合并
-            int[] store = new int[reverse(secret).length + secretKey.length];
+            long[] store = new long[reverse(secret).length + secretKey.length];
             for (int i = 0; i < store.length; i++) {
                 if (i < reverse(secret).length) {
                     store[i] = reverse(secret)[i];
@@ -65,12 +65,12 @@ public class ThreeTimesSetSecret {
             }
             return store;
         }
-       return new int[]{-1,-1};
+       return new long[]{-1,-1};
     }
     //实现数组里所有整数全部随机加或乘key
-    public int[] result2(int password, int key){
-        int[]p=parse(password);
-       int[]store=result1(password,p);
+    public long[] result2(long password, int key){
+        long[] p=parse(password);
+        long[]store=result1(password,p);
        for(int i=0;i<store.length;i++){
            store[i]=store[i]+key;
        }
@@ -78,7 +78,6 @@ public class ThreeTimesSetSecret {
     }
     public static void main(String[]asd){
         ThreeTimesSetSecret w=new ThreeTimesSetSecret();
-        System.out.println(Arrays.toString(w.result2(45721460, 2)));
-
+        System.out.println(Arrays.toString(w.result2(15175601727L, 2)));
     }
 }
